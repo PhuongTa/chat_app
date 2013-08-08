dd5a58ddfcba1f7dacde49d7e9b9664b6346a0ff
PhuongTa <tp.taphuong@gmail.com>
2013-08-09 02:46:49 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
+  
+  def index  	
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user= User.new(params[:user])
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save
+      flash[:success] = "Welome to the  Chat app!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
